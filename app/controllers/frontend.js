94f14eda525b1b00886403927eebc7c7b40bd619
MacgyverMartins <macgyver.martins@gmail.com>
2015-11-02 22:58:33 -0300

    enviando context para front-end

--- app/controllers/frontend.js
+++ app/controllers/frontend.js
@@ -3,19 +3,28 @@ var fs = require('fs');
 
 var frontendDir = 'frontend/';
 
-function filePath(name, prefix) {
-  var arquivo;
-  if (prefix) {
-    return arquivo = frontendDir + prefix + '-' + name + '.hbs';
-  }
-  return arquivo = name + '.hbs';
-}
-
 module.exports = function(app) {
   var PageController = {
 
     index: function(req, res) {
-      res.render('index');
+      var context = {
+        website: {
+          name: 'Creative - Start Bootstrap Theme',
+          description: 'Your Favorite Source of Free Bootstrap Themes'
+        },
+        page: {
+          title: 'Start Bootstrap',
+          content: '',
+          sections: [{
+            title: 'Your Favorite Source of Free Bootstrap Themes',
+            content: 'Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!'
+          }, {
+            title: "We've got what you need!",
+            content: 'Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!'
+          }, ]
+        }
+      }
+      res.render('index', context);
     },
 
     page: function(req, res) {
