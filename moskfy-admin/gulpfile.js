7eeb685977187407ded31aad05045000e39f83e6
MacgyverMartins <macgyver.martins@gmail.com>
2015-11-16 22:59:54 -0300

    Using the gulpfile structure from react-app-boilerplate with some modifications

--- moskfy-admin/gulpfile.js
+++ moskfy-admin/gulpfile.js
@@ -1,171 +1,214 @@
+// We need a bunch of dependencies, but they all do an important
+// part of this workflow
 var gulp = require('gulp');
-
-var $ = require('gulp-load-plugins')();
-var del = require('del');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
-var preprocessify = require('preprocessify');
-var runSequence = require('run-sequence');
-var domain = require('domain');
-
-var historyApiFallback = require('connect-history-api-fallback');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var notify = require('gulp-notify');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+var glob = require('glob');
+var livereload = require('gulp-livereload');
+var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
 var connect = require('gulp-connect');
 
-var env = 'dev';
-var webserver = false;
+// We create an array of dependencies. These are NPM modules you have
+// installed in node_modules. Think: "require('react')" or "require('underscore')"
+var dependencies = [
+    //'react' // react is part of this boilerplate
+];
 
-log = function(task, start) {
-  if (!start) {
-    setTimeout(function() {
-      $.util.log('Starting', '\'' + $.util.colors.cyan(task) + '\'...');
-    }, 1);
-  } else {
-    var time = ((new Date() - start) / 1000).toFixed(2) + ' s';
-    $.util.log('Finished', '\'' + $.util.colors.cyan(task) + '\'', 'after', $.util.colors.magenta(time));
+// Now this task both runs your workflow and deploys the code,
+// so you will see "options.development" being used to differenciate
+// what to do
+var browserifyTask = function (options) {
+
+  /* First we define our application bundler. This bundle is the
+     files you create in the "app" folder */
+    var appBundler = browserify({
+        entries: [options.src], // The entry file, normally "main.js"
+        extensions: ['.jsx', '.js'],
+        transform: [reactify], // Convert JSX style
+        debug: options.development, // Sourcemapping
+        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+    });
+
+    /* We set our dependencies as externals of our app bundler.
+     For some reason it does not work to set these in the options above */
+  appBundler.external(options.development ? dependencies : []);
+
+  /* This is the actual rebundle process of our application bundle. It produces
+    a "main.js" file in our "build" folder. */
+  var rebundle = function () {
+    var start = Date.now();
+    console.log('Building APP bundle');
+    appBundler.bundle()
+      .on('error', gutil.log)
+      .pipe(source('main.js'))
+      .pipe(gulpif(!options.development, streamify(uglify())))
+      .pipe(gulp.dest(options.dest))
+      .pipe(gulpif(options.development, livereload())) // It notifies livereload about a change if you use it
+      .pipe(notify(function () {
+        console.log('APP bundle built in ' + (Date.now() - start) + 'ms');
+      }))
+      .pipe(connect.reload());
+  };
+
+  /* When we are developing we want to watch for changes and
+    trigger a rebundle */
+  if (options.development) {
+    appBundler = watchify(appBundler);
+    appBundler.on('update', rebundle);
   }
-};
 
-gulp.task('clean:dev', function() {
-  return del(['.tmp']);
-});
+  // And trigger the initial bundling
+  rebundle();
 
-gulp.task('clean:dist', function() {
-  return del(['dist']);
-});
+  if (options.development) {
 
-gulp.task('scripts', function() {
-  var dev = env === 'dev';
-  var filePath = './app/scripts/app.js';
-  var extensions = ['.jsx', 'js'];
+    // We need to find all our test files to pass to our test bundler
+    var testFiles = glob.sync('./specs/**/*-spec.js');
 
-  var bundle = function() {
-    if (dev) {
-      var start = new Date();
-      log('scripts:bundle');
+    /* This bundle will include all the test files and whatever modules
+       they require from the application */
+    var testBundler = browserify({
+      entries: testFiles,
+      debug: true,
+      transform: [reactify],
+      cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+    });
+
+    // Again we tell this bundle about our external dependencies
+    testBundler.external(dependencies);
+
+    /* Now this is the actual bundle process that ends up in a "specs.js" file
+      in our "build" folder */
+    var rebundleTests = function () {
+      var start = Date.now();
+      console.log('Building TEST bundle');
+      testBundler.bundle()
+        .on('error', gutil.log)
+        .pipe(source('specs.js'))
+        .pipe(gulp.dest(options.dest))
+        .pipe(livereload()) // Every time it rebundles it triggers livereload
+        .pipe(notify(function () {
+          console.log('TEST bundle built in ' + (Date.now() - start) + 'ms');
+        }));
+    };
+
+    // We watch our test bundle
+    testBundler = watchify(testBundler);
+
+    // We make sure it rebundles on file change
+    testBundler.on('update', rebundleTests);
+
+    // Then we create the first bundle
+    rebundleTests();
+
+    /* And now we have to create our third bundle, which are our external dependencies,
+      or vendors. This is React JS, underscore, jQuery etc. We only do this when developing
+      as our deployed code will be one file with all application files and vendors */
+    var vendorsBundler = browserify({
+      debug: true, // It is nice to have sourcemapping when developing
+      require: dependencies
+    });
+
+    /* We only run the vendor bundler once, as we do not care about changes here,
+      as there are none */
+    var start = new Date();
+    console.log('Building VENDORS bundle');
+    vendorsBundler.bundle()
+      .on('error', gutil.log)
+      .pipe(source('vendors.js'))
+      .pipe(gulpif(!options.development, streamify(uglify())))
+      .pipe(gulp.dest(options.dest))
+      .pipe(notify(function () {
+        console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
+      }));
+
+  }
+
+}
+
+// We also have a simple css task here that you can replace with
+// SaSS, Less or whatever
+var cssTask = function (options) {
+    if (options.development) {
+      var run = function () {
+        gulp.src(options.src)
+          .pipe(concat('main.css'))
+          .pipe(gulp.dest(options.dest));
+      };
+      run();
+      gulp.watch(options.src, run);
+    } else {
+      gulp.src(options.src)
+        .pipe(concat('main.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest(options.dest));
     }
+}
 
-    var appBundle = browserify({
-      entries: [filePath],
-      extensions: extensions,
-      debug: env === 'dev'
-    })
-
-    appBundle.external(['react', 'react-router', 'material-ui'])
-
-    appBundle.transform("babelify", {
-        presets: [
-          //{ plugins: ["syntax-class-properties"] },
-          "es2015",
-          "react"
-        ]
-      })
-      .bundle()
-      .pipe(source('app.js'))
-      .pipe(gulp.dest('.tmp/scripts/bundle'))
-      .pipe($.if(dev, $.tap(function() {
-        log('scripts:bundle', start);
-        connect.reload()
-      })));
-  }
-
-  bundle();
-
-  //if (dev) {
-  //gulp.src(filePath)
-  //.pipe($.plumber())
-  //.pipe($.tap(function(file) {
-  //var d = domain.create();
-
-  //d.on('error', function(err) {
-  //$.util.log($.util.colors.red('Browserify compile error:'), err.message, '\n\t', $.util.colors.cyan('in file'), file.path);
-  //$.util.beep();
-  //});
-
-  //d.run(bundle);
-  //}));
-  //} else {
-  //bundle();
-  //}
+gulp.task('watch', function () {
+  gulp.watch(['./app/scripts/**/*.js'], function(){
+    browserifyTask({
+      development: true,
+      src: './app/scripts/app.js',
+      dest: './.tmp/scripts/bundle'
+    });
+  });
 });
-
-
-gulp.task('imagemin', function() {
-  return gulp.src('app/images/*')
-    .pipe($.imagemin({
-      progressive: true,
-      svgoPlugins: [{
-        removeViewBox: false
-      }]
-    }))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('copy', function() {
-  return gulp.src(['app/*.txt', 'app/*.ico'])
-    .pipe(gulp.dest('dist'));
-})
-
-//gulp.task('bundle', function() {
-//var assets = $.useref.assets();
-//var revAll = new $.revAll({
-//dontRenameFile: [/^\/favicon.ico$/g, '.html']
-//});
-//var jsFilter = $.filter(['**/*.js']);
-//var cssFilter = $.filter(['**/*.css']);
-//var htmlFilter = $.filter(['*.html']);
-
-//return gulp.src('app/index.html')
-//.pipe($.preprocess())
-//.pipe(assets)
-//.pipe(assets.restore())
-//.pipe($.useref())
-//.pipe(jsFilter)
-//.pipe($.uglify())
-//.pipe(jsFilter.restore())
-//.pipe(cssFilter)
-//.pipe($.autoprefixer({
-//browsers: ['last 5 versions']
-//}))
-//.pipe($.minifyCss())
-//.pipe(cssFilter.restore())
-//.pipe(htmlFilter)
-//.pipe($.htmlmin({
-//collapseWhitespace: true
-//}))
-//.pipe(htmlFilter.restore())
-//.pipe(revAll.revision())
-//.pipe(gulp.dest('dist'))
-//.pipe($.size());
-//});
 
 gulp.task('connect', function() {
   connect.server({
     root: ['app', '.tmp'],
     port: 3333,
-    //base: config.devBaseUrl,
-    livereload: true,
-    middleware: function(connect, opt) {
-      return [historyApiFallback({})];
-    }
+    livereload: true
   });
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['./app/*.html', './app/scripts/**/*.js', './app/scripts/**/*.jsx'], ['scripts']);
+// Starts our development workflow
+gulp.task('default', ['connect', 'watch'], function () {
+
+  browserifyTask({
+    development: true,
+    src: './app/scripts/app.js',
+    dest: './.tmp/scripts/bundle'
+  });
+
+  //cssTask({
+    //development: true,
+    //src: './styles/**/*.css',
+    //dest: './build'
+  //});
+
 });
 
-gulp.task('serve', ['scripts'], function() {
-  runSequence('clean:dev', ['connect']);
-  gulp.watch('app/*.html');
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/scripts/**/*.jsx', ['scripts']);
+// Deploys code to our "dist" folder
+//gulp.task('deploy', function () {
+
+  //browserifyTask({
+    //development: false,
+    //src: './app/main.js',
+    //dest: './dist'
+  //});
+
+  //cssTask({
+    //development: false,
+    //src: './styles/**/*.css',
+    //dest: './dist'
+  //});
+
+//});
+
+// Runs the test with phantomJS and produces XML files
+// that can be used with f.ex. jenkins
+gulp.task('test', function () {
+    return gulp.src('./build/testrunner-phantomjs.html').pipe(jasminePhantomJs());
 });
-
-gulp.task('build', function() {
-  env = 'prod';
-  runSequence(['clean:dev', 'clean:dist'], ['scripts', 'imagemin'],
-    'bundle', 'copy');
-});
-
-gulp.task('default', ['build']);
-
